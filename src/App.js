21362bd8809b8e1dc2ba6d3f34d19f18d0afa406
pahalsonu <pahalsonu@gmail.com>
2021-01-25 17:28:22 +0530

    carousel is converted but need to add buttons

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Carousel from './component/Carousel'
 import Navbar from './component/Navbar'
+import Strapcarousel from './component/Strapcarousel'
 class App extends React.Component {
     render() {
         return (
             <div >
                 <Navbar />
+                <Strapcarousel  />
                 <Carousel />
 
             </div>
---
+++ src/component/Strapcarousel.js
@@ -0,0 +1,94 @@
+
+
+import React, { useState } from 'react';
+import {
+    Carousel,
+    CarouselItem,
+    CarouselControl,
+    CarouselIndicators,
+    CarouselCaption
+} from 'reactstrap';
+
+const items = [
+    {
+        altText: 'Example headline.',
+        caption: 'Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit',
+        header: 'Example headline. ',
+        key: '1'
+    },
+    {
+        altText: 'Example headline.',
+        caption: 'Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit',
+        header: 'Example headline. ',
+        key: '2'
+    },
+    {
+        altText: 'Example headline.',
+        caption: 'Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit',
+        header: 'Example headline.',
+        key: '3'
+    }
+];
+
+const Example = (props) => {
+    const [activeIndex, setActiveIndex] = useState(0);
+    const [animating, setAnimating] = useState(false);
+
+    const next = () => {
+        if (animating) return;
+        const nextIndex = activeIndex === items.length - 1 ? 0 : activeIndex + 1;
+        setActiveIndex(nextIndex);
+    }
+
+    const previous = () => {
+        if (animating) return;
+        const nextIndex = activeIndex === 0 ? items.length - 1 : activeIndex - 1;
+        setActiveIndex(nextIndex);
+    }
+
+    const goToIndex = (newIndex) => {
+        if (animating) return;
+        setActiveIndex(newIndex);
+    }
+
+    const slides = items.map((item) => {
+        return (
+            <CarouselItem
+                className="custom-tag"
+                tag="div"
+                key={item.key}
+                onExiting={() => setAnimating(true)}
+                onExited={() => setAnimating(false)}
+            >
+                <CarouselCaption className="text-light" captionText={item.altText} captionHeader={item.altText} />
+                
+            </CarouselItem>
+        );
+    });
+
+    return (
+        <div>
+            <style>
+                {
+                    `.custom-tag {
+              max-width: 100%;
+              height: 500px;
+              background: grey;
+            }`
+                }
+            </style>
+            <Carousel
+                activeIndex={activeIndex}
+                next={next}
+                previous={previous}
+            >
+                <CarouselIndicators items={items} activeIndex={activeIndex} onClickHandler={goToIndex} />
+                {slides}
+                <CarouselControl direction="prev" directionText="Previous" onClickHandler={previous} />
+                <CarouselControl direction="next" directionText="Next" onClickHandler={next} />
+            </Carousel>
+        </div>
+    );
+}
+
+export default Example;
