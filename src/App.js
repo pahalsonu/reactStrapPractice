2727c2c5e016bbfea1c73462fbcd98f33830eb6d
pahalsonu <pahalsonu@gmail.com>
2021-01-25 15:58:26 +0530

    carousel bootsrap form is placed and working

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
-
-class  App extends React.Component {
-  render(){
-    return(
-      <h1>Hello World</h1>
-    )
-  }
+import Carousel from './Carousel'
+class App extends React.Component {
+    render() {
+        return (
+            <div >
+                <h1>Hello World</h1>
+                <Carousel />
+            </div>
+        )
+    }
 
 }
 export default App;
---
+++ src/Carousel.js
@@ -0,0 +1,148 @@
+import React, { Component } from 'react'
+
+export default class Carousel extends Component {
+    render() {
+        return (
+            <div>
+        <header>
+          <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+            <a className="navbar-brand" href="#">Carousel</a>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon" />
+            </button>
+            <div className="collapse navbar-collapse" id="navbarCollapse">
+              <ul className="navbar-nav mr-auto">
+                <li className="nav-item active">
+                  <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">Link</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link disabled" href="#">Disabled</a>
+                </li>
+              </ul>
+              <form className="form-inline mt-2 mt-md-0">
+                <input className="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search" />
+                <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+              </form>
+            </div>
+          </nav>
+        </header>
+        <main role="main">
+          <div id="myCarousel" className="carousel slide" data-ride="carousel">
+            <ol className="carousel-indicators">
+              <li data-target="#myCarousel" data-slide-to={0} className="active" />
+              <li data-target="#myCarousel" data-slide-to={1} />
+              <li data-target="#myCarousel" data-slide-to={2} />
+            </ol>
+            <div className="carousel-inner">
+              <div className="carousel-item active">
+                <img className="first-slide" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="First slide" />
+                <div className="container">
+                  <div className="carousel-caption text-left">
+                    <h1>Example headline.</h1>
+                    <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+                    <p><a className="btn btn-lg btn-primary" href="#" role="button">Sign up today</a></p>
+                  </div>
+                </div>
+              </div>
+              <div className="carousel-item">
+                <img className="second-slide" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Second slide" />
+                <div className="container">
+                  <div className="carousel-caption">
+                    <h1>Another example headline.</h1>
+                    <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+                    <p><a className="btn btn-lg btn-primary" href="#" role="button">Learn more</a></p>
+                  </div>
+                </div>
+              </div>
+              <div className="carousel-item">
+                <img className="third-slide" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Third slide" />
+                <div className="container">
+                  <div className="carousel-caption text-right">
+                    <h1>One more for good measure.</h1>
+                    <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+                    <p><a className="btn btn-lg btn-primary" href="#" role="button">Browse gallery</a></p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <a className="carousel-control-prev" href="#myCarousel" role="button" data-slide="prev">
+              <span className="carousel-control-prev-icon" aria-hidden="true" />
+              <span className="sr-only">Previous</span>
+            </a>
+            <a className="carousel-control-next" href="#myCarousel" role="button" data-slide="next">
+              <span className="carousel-control-next-icon" aria-hidden="true" />
+              <span className="sr-only">Next</span>
+            </a>
+          </div>
+          {/* Marketing messaging and featurettes
+      ================================================== */}
+          {/* Wrap the rest of the page in another container to center all the content. */}
+          <div className="container marketing">
+            {/* Three columns of text below the carousel */}
+            <div className="row">
+              <div className="col-lg-4">
+                <img className="rounded-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width={140} height={140} />
+                <h2>Heading</h2>
+                <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
+                <p><a className="btn btn-secondary" href="#" role="button">View details »</a></p>
+              </div>{/* /.col-lg-4 */}
+              <div className="col-lg-4">
+                <img className="rounded-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width={140} height={140} />
+                <h2>Heading</h2>
+                <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.</p>
+                <p><a className="btn btn-secondary" href="#" role="button">View details »</a></p>
+              </div>{/* /.col-lg-4 */}
+              <div className="col-lg-4">
+                <img className="rounded-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width={140} height={140} />
+                <h2>Heading</h2>
+                <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+                <p><a className="btn btn-secondary" href="#" role="button">View details »</a></p>
+              </div>{/* /.col-lg-4 */}
+            </div>{/* /.row */}
+            {/* START THE FEATURETTES */}
+            <hr className="featurette-divider" />
+            <div className="row featurette">
+              <div className="col-md-7">
+                <h2 className="featurette-heading">First featurette heading. <span className="text-muted">It'll blow your mind.</span></h2>
+                <p className="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+              </div>
+              <div className="col-md-5">
+                <img className="featurette-image img-fluid mx-auto" data-src="holder.js/500x500/auto" alt="Generic placeholder image" />
+              </div>
+            </div>
+            <hr className="featurette-divider" />
+            <div className="row featurette">
+              <div className="col-md-7 order-md-2">
+                <h2 className="featurette-heading">Oh yeah, it's that good. <span className="text-muted">See for yourself.</span></h2>
+                <p className="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+              </div>
+              <div className="col-md-5 order-md-1">
+                <img className="featurette-image img-fluid mx-auto" data-src="holder.js/500x500/auto" alt="Generic placeholder image" />
+              </div>
+            </div>
+            <hr className="featurette-divider" />
+            <div className="row featurette">
+              <div className="col-md-7">
+                <h2 className="featurette-heading">And lastly, this one. <span className="text-muted">Checkmate.</span></h2>
+                <p className="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+              </div>
+              <div className="col-md-5">
+                <img className="featurette-image img-fluid mx-auto" data-src="holder.js/500x500/auto" alt="Generic placeholder image" />
+              </div>
+            </div>
+            <hr className="featurette-divider" />
+            {/* /END THE FEATURETTES */}
+          </div>{/* /.container */}
+          {/* FOOTER */}
+          <footer className="container">
+            <p className="float-right"><a href="#">Back to top</a></p>
+            <p>© 2017-2018 Company, Inc. · <a href="#">Privacy</a> · <a href="#">Terms</a></p>
+          </footer>
+        </main>
+            </div>
+        )
+    }
+}
